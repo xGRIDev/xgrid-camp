f9a6532e6906474f7d75c5ac34c10e579cf6e2bc
Zufarmhsnnfl <zufarnaufal8@gmail.com>
2024-02-12 14:47:12 +0700

    UPDATE

--- app/Models/Checkout.php
+++ app/Models/Checkout.php
@@ -10,5 +10,10 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Checkout extends Model
 {
     use HasFactory, SoftDeletes;
-    protected $fillable = ['user_id', 'camp_id', 'card_number', 'expired', 'is_paid'];
+    protected $fillable = ['user_id', 'camp_id', 'card_number', 'expired', 'is_paid', 'cvc'];
+
+    public function setExpiredAttribute($value)
+    {
+        $this->attributes['expired'] = date('Y-m-t', strtotime($value));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-
+use App\Http\Controllers\User\CheckoutController;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::get('/checkout', function () {
-    return view('checkout');
-})->name('checkout');
-
-Route::get('/success-checkout', function () {
-    return view('success_checkout');
-})->name('success-checkout');
+Route::get('checkout/success', [CheckoutController::class, 'success'])->name('checkout.success');
+Route::get('checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create');
+Route::post('checkout/{camp}', [CheckoutController::class, 'store'])->name('checkout.store');
 
 Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
 Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user.google.callback');
